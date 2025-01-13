352ee8d3f83e475d5e75b172a1f10b76f50b69f2
Matheus <matheusdelpinocabral@gmail.com>
2025-01-13 16:02:49 -0300

    iniciando testes de metodos do AutomationWeb

---
+++ SeleniumBot.AutomationWeb.Tests/AutomationWebMethodsTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+
+namespace SeleniumBot.AutomationWeb.Tests;
+
+public class AutomationWebMethodsTest
+{
+
+    public IWebDriver? driver;
+    public WebDriverWait wait;
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+
+    [TearDown]
+    public void TearDown()
+    {
+    }
+
+
+    [Test]
+    public void FindNameNotNullTest()
+    {
+        // arrange
+
+        // act
+
+        // assert
+
+    }
+
+}
